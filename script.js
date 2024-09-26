1ba5e864a4ff87dbf9816838a816d50b171bd6d7
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-09-26 20:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function shortcut(s1, s2) { 
 	if (s1.length > 0 && s2.length > 0) {
-    return s1[0].toUpperCase() + s2[0].toLowerCase();
+    return s1[0].toLowerCase() + s2[0].toUpperCase();
   }
-  return '';
+  return ''
   // your code here
 }
 
