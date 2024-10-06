13c4b507f6d207bb973cdbc86bf963dcc944dccc
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-06 17:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function shortcut(s1, s2) { 
-	if (s1.length > 0 && s2.length > 0) {
+  if (s1.length > 0 && s2.length > 0) {
     return s1[0].toLowerCase() + s2[0].toUpperCase();
   }
-  return ''
-  // your code here
+  return '';
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+
