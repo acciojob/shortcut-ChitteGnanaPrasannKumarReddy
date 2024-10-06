fe9797b44c84bb95d0382a9b1366598f8935884d
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-06 17:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function shortcut(s1, s2) { 
   if (s1.length > 0 && s2.length > 0) {
-    return s1[0].toLowerCase() + s2[0].toUpperCase();
+    return s1[0] + s2[0];
   }
   return '';
+
+  // your code here
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
-
